1245159185dddbb90cd1af0f05d90b3f189ad18b
loshaniramsha <loshanikottagoda@gmail.com>
2024-08-26 20:58:54 +0530

    implement CustomerBOImpl

--- src/main/java/org/example/aadassignment01backend/bo/custom/impl/CustomerBOImpl.java
+++ src/main/java/org/example/aadassignment01backend/bo/custom/impl/CustomerBOImpl.java
@@ -1,12 +1,16 @@
 package org.example.aadassignment01backend.bo.custom.impl;
 
 import org.example.aadassignment01backend.bo.custom.CustomerBO;
+import org.example.aadassignment01backend.dao.DAOFactory;
+import org.example.aadassignment01backend.dao.custom.CustomerDAO;
 import org.example.aadassignment01backend.dto.CustomerDTO;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class CustomerBOImpl implements CustomerBO {
+
+    CustomerDAO customerDAO= (CustomerDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.CUSTOMER);
     @Override
     public ArrayList<CustomerDTO> getAllCustomers() throws SQLException, ClassNotFoundException {
         return null;
