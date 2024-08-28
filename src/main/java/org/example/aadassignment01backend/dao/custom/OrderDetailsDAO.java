79a7867485efce68677b249600c2252ee1708ee8
loshaniramsha <loshanikottagoda@gmail.com>
2024-08-28 07:30:06 +0530

    Changes OrderDetailsDAO interface

--- src/main/java/org/example/aadassignment01backend/dao/custom/OrderDetailsDAO.java
+++ src/main/java/org/example/aadassignment01backend/dao/custom/OrderDetailsDAO.java
@@ -1,12 +1,13 @@
 package org.example.aadassignment01backend.dao.custom;
 
 import org.example.aadassignment01backend.dao.CrudDAO;
+import org.example.aadassignment01backend.dao.SuperDAO;
 import org.example.aadassignment01backend.entity.OrderDetails;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public interface OrderDetailsDAO extends CrudDAO<OrderDetails> {
+public interface OrderDetailsDAO extends SuperDAO {
     boolean save(OrderDetails dto) throws SQLException, ClassNotFoundException;
     ArrayList<OrderDetails> searchByOrderId(String id) throws SQLException, ClassNotFoundException;
 }
