d4c3180a8dbd850ff85cff1121c86e5de9d02bae
loshaniramsha <loshanikottagoda@gmail.com>
2024-08-28 08:50:13 +0530

    implement OrderDetailsBOImpl

--- src/main/java/org/example/aadassignment01backend/bo/custom/impl/OrderDetailsBOImpl.java
+++ src/main/java/org/example/aadassignment01backend/bo/custom/impl/OrderDetailsBOImpl.java
@@ -1,19 +1,28 @@
 package org.example.aadassignment01backend.bo.custom.impl;
 
 import org.example.aadassignment01backend.bo.custom.OrderDetailsBO;
+import org.example.aadassignment01backend.dao.DAOFactory;
+import org.example.aadassignment01backend.dao.custom.OrderDetailsDAO;
 import org.example.aadassignment01backend.dto.OrderDetailsDTO;
+import org.example.aadassignment01backend.entity.OrderDetails;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class OrderDetailsBOImpl implements OrderDetailsBO {
+    OrderDetailsDAO orderDetailsDAO= (OrderDetailsDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.ORDERDETAILS);
     @Override
     public boolean save(OrderDetailsDTO dto) throws SQLException, ClassNotFoundException {
-        return false;
+        return orderDetailsDAO.save(new OrderDetails(dto.getOrderId(),dto.getItemId(),dto.getQty(),dto.getUnitPrice(),dto.getTotal()));
     }
 
     @Override
     public ArrayList<OrderDetailsDTO> searchByOrderId(String id) throws SQLException, ClassNotFoundException {
-        return null;
+       ArrayList<OrderDetails> search=orderDetailsDAO.searchByOrderId(id);
+       ArrayList<OrderDetailsDTO> orderDetailsDTOS=new ArrayList<>();
+       for (OrderDetails orderDetails:search){
+           orderDetailsDTOS.add(new OrderDetailsDTO(orderDetails.getOrderId(),orderDetails.getItemId(),orderDetails.getQty(),orderDetails.getUnitPrice(),orderDetails.getTotal()));
+       }
+       return orderDetailsDTOS;
     }
 }
